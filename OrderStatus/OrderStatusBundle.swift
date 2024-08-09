28add93f5a76c39adf95640fb08ef9bd32d475f7
shubham nanda <shubhamn.ec.22@gmail.com>
2024-08-09 16:40:36 +0530

    update

---
+++ OrderStatus/OrderStatusBundle.swift
@@ -0,0 +1,17 @@
+//
+//  OrderStatusBundle.swift
+//  OrderStatus
+//
+//  Created by Shubham Nanda on 24/07/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct OrderStatusBundle: WidgetBundle {
+    var body: some Widget {
+        OrderStatus()
+        OrderStatusLiveActivity()
+    }
+}
